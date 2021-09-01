4d1229e006d80fc2988bc0c271abad96586a01b6
MoBumShu <bumsoot@hanmail.net>
2021-09-01 16:09:16 +0900

    Update 3.4.3~.py

--- 3.4.3~.py
+++ 3.4.3~.py
@@ -50,3 +50,47 @@ partial_y_train = y_train[10000:]
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size= 512, validation_data=(x_val, y_val))
+
+#훈련과 검증 손실 그리기
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(loss)+1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+#훈련과 검증 정확도 그리기
+
+plt.clf()
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
+
+#4번쨰 에포크 이후로 오버피팅 떄문에 정확한 값이 나오지 않으므로 모델을 처음부터 다시 훈련하겠다.
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)

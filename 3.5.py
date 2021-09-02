4e38f7023926cf869f22420b9def97e9e470895b
MoBumShu <bumsoot@hanmail.net>
2021-09-02 17:45:19 +0900

    Update 3.5.py

--- 3.5.py
+++ 3.5.py
@@ -2,12 +2,12 @@
 #로이터 데이터셋 로드하기
 from tensorflow.keras.datasets import reuters
 
-(train_data, train_labels), (test_data, test_labels) = reuters.lad_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
 #로이터 데이터셋을 텍스트로 디코딩하기
 word_index = reuters.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-decorded_newswire = ''.join([reverse_word_index.get(i -3, '?') for i in train_data])
+decorded_newswire = ' '.join([reverse_word_index.get(i -3, '?') for i in train_data[0]])
 
 #데이터 인코딩하기 -> 원핫인코딩(범주형 인코딩): 각 레이블의 인덱스 자리는 1이고 나머지는 모두 0인 벡터
 import numpy as np
@@ -17,6 +17,15 @@ def vectorize_sequences(sequences, dimension=10000):
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
     return results
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1.
+    return results
+
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
@@ -38,11 +47,34 @@ x_val = x_train[:1000]
 partial_x_train = x_train[1000:]
 
 y_val = one_hot_train_labels[:1000]
-partial_y_train = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
 
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
 
 #훈련과 검증 손실 그리기
 import matplotlib.pyplot as plt
 
-loss = history.hi
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(partial_x_train, partial_y_train, epochs=9, batch_size=512, validation_data=(x_val, y_val))
+results = model.evaluate(x_test, one_hot_test_labels)
+print(results)
+
+predictions = model.predict(x_test)
